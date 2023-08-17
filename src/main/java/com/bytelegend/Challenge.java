fcd757654e45c0f796dd8221d803fd9ebb056638
yb1959080058 <1959080058@qq.com>
2023-08-17 06:57:24 +0000

    Update at 2023-08-17T06:57:24.848423Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,18 @@ package com.bytelegend;
 
 public class Challenge {
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1+1=" + add(1, 1));
+    }
+}
+package com.bytelegend;
+
+public class Challenge {
+    public static int add(int a, int b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
